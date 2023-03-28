bcf57d476d0f32f94e0fcfae22396a7ab2857eb6
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-03-28 13:23:43 +0530

    Updated cors

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "*" }));
+app.use(cors({ 
+   credentials: true;
+   origin: [
+    "http://localhost:3000",
+    "https://vcare.onrender.com"
+]
+ }));
 
 require("dotenv").config({ path: ".env" });
 
